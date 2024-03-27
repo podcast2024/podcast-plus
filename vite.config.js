4f853c744810142a45d2b05be1cb08821f8c4435
podcast2024 <podcast2024@163.com>
2024-03-27 23:31:03 +0800

    push

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 import px2rem from 'postcss-px2rem'
 
 export default defineConfig({
-    // publicDir: '/',
+    base:'./',
     css: {
 	postcss: {
 	    plugins: [
